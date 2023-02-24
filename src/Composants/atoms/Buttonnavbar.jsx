f49fd44d264f9de1e474a54c4eae97b7333c4b45
Mira-morisho <morishomira@gmail.com>
2023-02-24 10:36:50 +0100

    fix:mofifier le boutton mail

--- src/Composants/atoms/Buttonnavbar.jsx
+++ src/Composants/atoms/Buttonnavbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../../styles/Header.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Buttonnavbar() {
   return (
     <div>
       <button type="submit" className="mail">
-        {" "}
-        challim@kinshasadigital.com
+        <a href="mailto:challim@kinshasadigital.com?subject=feedback">
+          challim@kinshadadigital.com
+        </a>
       </button>
     </div>
   );
