cf30f3a19ec4a4827c848a43d3efe16cd953331e
Mira-morisho <morishomira@gmail.com>
2023-03-01 11:17:15 +0100

    supprimer le composant Acceuil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Composants/organisms/Header";
-import Acceuil from "./Composants/organisms/Acceuil";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Acceuil />
     </div>
   );
 }
