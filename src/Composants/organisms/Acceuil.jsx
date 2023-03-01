55f115d5c4d70cd12d7427bd7f7180572850bf0b
Mira-morisho <morishomira@gmail.com>
2023-03-01 13:53:59 +0100

    fix:modifier le composant cc

---
+++ src/Composants/organisms/Acceuil.jsx
@@ -0,0 +1,10 @@
+import "../../styles/Header.css";
+import Name from "../molecules/Name";
+
+export default function Acceuil() {
+  return (
+    <div className="acceuil">
+      <Name />
+    </div>
+  );
+}
