f81d22b03fc227e844a42d3ac3fd5e0027513910
Mira-morisho <morishomira@gmail.com>
2023-03-12 20:00:41 +0100

    fix:modifierle composant name responsive

--- src/Composants/molecules/Name.jsx
+++ src/Composants/molecules/Name.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../styles/Acceuil.css";
 
+// import { AiOutlineArrowUp, } from "react-icons/ai";
+
 export default function () {
   return (
     <div>
       <div className="contenair">
-        <h1 className="name"> CHALLIM MUSOMBO </h1>
+        <h1 className="name"> CHALLIM MUSMBO</h1>
         <p className="description">
           I create engaging user experience design for brands and startups by
           balancing the business needs and the user's needs.
