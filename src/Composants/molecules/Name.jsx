278746ff1b0e152a54a845fe79ff42cb57508ea4
Mira-morisho <morishomira@gmail.com>
2023-03-14 16:10:41 +0100

    fix:resposiver de la page d'acceuil

--- src/Composants/molecules/Name.jsx
+++ src/Composants/molecules/Name.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "../../styles/Acceuil.css";
 
-// import { AiOutlineArrowUp, } from "react-icons/ai";
-
 export default function () {
   return (
     <div>
       <div className="contenair">
-        <h1 className="name"> CHALLIM MUSMBO</h1>
+        <h1 className="name"> CHALLIM MUSOMBO </h1>
         <p className="description">
-          I create engaging user experience design for brands and startups by
-          balancing the business needs and the user's needs.
+          A la recherche d'un nouveau challenge, je peux apporter des réponses
+          évidentes face aux défis auxquels votre entreprise est confrontée .
         </p>
       </div>
       <div className="video"> </div>
