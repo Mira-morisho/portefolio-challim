16223d0ff0ee45347fbfcf64c6156369b298cf34
Mira-morisho <morishomira@gmail.com>
2023-03-29 11:15:04 +0100

    fix: styliser le composant portefolio

--- src/Composants/organisms/MyPortefolio.jsx
+++ src/Composants/organisms/MyPortefolio.jsx
@@ -1,15 +1,45 @@
 import React from "react";
 import "../../styles/Portefolio.css";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function MyPortefolio() {
   return (
     <div className="contenairportefolio">
-      <div className="buttonportefolio">
-        <div className="portefoliovide">vvvvv</div>
-        <button className="portefolioboutton"> ALL CASE STUDE</button>
+      <div className="portefolio1">
+        <div className="boutonvide"></div>
+        <div className="boutonprofil">
+          <button className="bouton">ALL CASE STUDE</button>
+        </div>
       </div>
-      <div>
-        <div className="myportefolio"> vvvvvvvvvvvv</div>
+      <div className="portefolio2">
+        <div className="portefolio2pricipaldiv">
+          <div className="altridicone">
+            <h1 className="altrid">ALtrid</h1>
+            <div className="iconeportefolio">
+              <AiOutlineClose />
+            </div>
+          </div>
+          <p className="paragraportefolio">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod
+            lacinia turpis ac blandit. Suspendisse et ligula orci. Interdum et
+            malesuada fames ac ante ipsum primis in faucibus. Nam pellentesque
+            est sed ultricies tempor. Donec quis ex volutpat, blandit sem sed,
+            dictum est. Praesent faucibus justo at est sollicitudin, non
+            consequat sapien maximus. Vestibulum posuere mollis leo, non
+            ullamcorper eros commodo quis. Inter
+          </p>
+          <div className="mobilappli">MOBIL APPLICATION</div>
+        </div>
+      </div>
+      <div className="portefolio3">
+        <div className="portefolio3contenair">
+          <img
+            width={400}
+            height={372}
+            src="https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg"
+            className=" imgportefolio"
+          />
+        </div>
       </div>
     </div>
   );
