6082085586bd2bcfcb71ca889d8e48e21d1e7fb4
Mira-morisho <morishomira@gmail.com>
2023-04-06 23:05:38 +0100

    fix:ajout de la session client

--- src/Composants/molecules/PersonnageClient.jsx
+++ src/Composants/molecules/PersonnageClient.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Client.css";
 
-export default function PersonnageClient() {
-  return <div>PersonnageClient</div>;
+export default function PersonnageClient({ Parentclients }) {
+  const [numclient, setNumClient] = useState(0);
+  return (
+    <div>
+      PersonnageClient
+      <span
+        onClick={() => {
+          if (numclient > 0) {
+            setNumClient(numclient - 1);
+          }
+        }}
+      >
+        Precedant{" "}
+      </span>
+      <span
+        onClick={() => {
+          if (numclient <= 1) {
+            setNumClient(numclient + 1);
+          }
+        }}
+      >
+        Suivant{" "}
+      </span>
+      <p>{Parentclients[numclient].name}</p>
+      <p>{Parentclients[numclient].description}</p>
+      <img width={240} height={300} src={Parentclients[numclient].photo} />
+    </div>
+  );
 }
--- src/Composants/organisms/Client.jsx
+++ src/Composants/organisms/Client.jsx
@@ -4,10 +4,33 @@ import PersonnageClient from "../molecules/PersonnageClient";
 import "../../styles/Client.css";
 
 export default function Client() {
+  const clients = [
+    {
+      name: "Mira",
+      description:
+        "Moi mira safi  design smart, in-demand financial solutions, delighting the client’s audience with innovations in the finance sector. Putting end-users at the forefront and creating a high-end experience, I improve business metrics with designs.",
+      photo:
+        "https://www.kemiadeleke.com/static/media/proj-1.37196af1fdc6dda06072.png",
+    },
+    {
+      name: "   ASHA MORISHO",
+      description:
+        "ASHA  est un  design smart, in-demand financial solutions, delighting the client’s audience with innovations in the finance sector. Putting end-users at the forefront and creating a high-end experience, I improve business metrics with designs. safi est une dev fillstack patiskjkqjsq",
+      photo:
+        "https://www.kemiadeleke.com/static/media/proj-1.37196af1fdc6dda06072.png",
+    },
+    {
+      name: "MICOCO",
+      description:
+        "Bonjour  design smart, in-demand financial solutions, delighting the client’s audience with innovations in the finance sector. Putting end-users at the forefront and creating a high-end experience, I improve business metrics with designs. safi est une dev fillstack patiskjkqjsq",
+      photo:
+        "https://www.kemiadeleke.com/static/media/proj-1.37196af1fdc6dda06072.png",
+    },
+  ];
   return (
     <div className="client">
       <TitreClient />
-      <PersonnageClient />
+      <PersonnageClient Parentclients={clients} />
     </div>
   );
 }
